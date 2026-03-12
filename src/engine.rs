8283259c31f05d8e290a9856c3b70743aa0c7925
piot5 <binichhier@googlemail.com>
2026-03-12 14:05:12 +0100

    engine restored

--- src/engine.rs
+++ src/engine.rs
@@ -1,56 +1,26 @@
-use std::{mem, iter, thread, time::Duration};
+use std::{mem, iter};
 use chrono::Local;
 use windows::core::PCWSTR;
-use windows::Win32::Graphics::Gdi::*;
-use windows::Win32::Devices::Display::{SetDisplayConfig, SDC_APPLY, SDC_TOPOLOGY_EXTEND, SDC_TOPOLOGY_INTERNAL, SDC_ALLOW_CHANGES};
+use windows::Win32::Graphics::Gdi::{
+    EnumDisplaySettingsW, ChangeDisplaySettingsExW, DEVMODEW, ENUM_CURRENT_SETTINGS,
+    CDS_UPDATEREGISTRY, CDS_NORESET, CDS_SET_PRIMARY, DM_PELSWIDTH, DM_PELSHEIGHT, 
+    DM_POSITION, DM_DISPLAYORIENTATION, DM_DISPLAYFREQUENCY, CDS_TYPE, DISP_CHANGE_SUCCESSFUL,
+    DMDO_DEFAULT, DMDO_90, DMDO_180, DMDO_270, DISPLAY_DEVICEW, EnumDisplayDevicesW,
+    ENUM_REGISTRY_SETTINGS, DISP_CHANGE_RESTART
+};
+use windows::Win32::Devices::Display::{SetDisplayConfig, SDC_APPLY, SDC_TOPOLOGY_EXTEND, SDC_TOPOLOGY_CLONE};
 use crate::scraper::{self, DisplayTask, DisplayRow};
 
-pub struct DFEngine;
+pub struct DFEngine; 
 
 impl DFEngine {
-    pub fn new() -> Self { Self }
+    pub fn new() -> Self {
+        Self
+    }
 
     pub fn log(&self, tag: &str, msg: &str) {
         let ts = Local::now().format("%Y%m%dT%H%M%S");
         println!("[{}] [{}] {}", ts, tag, msg);
-    }
-
-    pub fn atomic_deploy(&self, tasks: Vec<DisplayTask>, _clones: Vec<(String, String)>) {
-        // 1. Nutze die bewährte Discovery-Funktion um das System vorzubereiten
-        let inv = self.full_scan_discovery();
-
-        let (to_activate, to_deactivate): (Vec<_>, Vec<_>) = tasks.into_iter()
-            .partition(|t| t.width > 0 && t.height > 0);
-
-        // PHASE A: Detach (BNQ aus)
-        for task in &to_deactivate {
-            if let Some(dev) = inv.iter().find(|r| self.match_display(r, &task.query)) {
-                self.force_set_registry(&dev.name_id, 0, 0, 0, 0, false);
-            }
-        }
-        self.commit_registry();
-
-        // PHASE B: Wunsch-Layout (TCL auf 1080p)
-        let mut sorted_active = to_activate;
-        sorted_active.sort_by_key(|t| !t.is_primary);
-
-        for task in &sorted_active {
-            if let Some(dev) = inv.iter().find(|r| self.match_display(r, &task.query)) {
-                self.force_set_registry(&dev.name_id, task.width, task.height, task.x, task.y, task.is_primary);
-            }
-        }
-
-        // PHASE C: Brute Force Hardware Apply
-        unsafe {
-            let _ = ChangeDisplaySettingsExW(None, None, None, CDS_TYPE(0), None);
-            thread::sleep(Duration::from_millis(250));
-            
-            let _ = SetDisplayConfig(
-                None, 
-                None, 
-                SDC_APPLY | SDC_ALLOW_CHANGES | SDC_TOPOLOGY_INTERNAL
-            );
-        }
     }
 
     pub fn full_scan_discovery(&self) -> Vec<DisplayRow> {
@@ -58,7 +28,7 @@ impl DFEngine {
         unsafe {
             let _ = SetDisplayConfig(None, None, SDC_APPLY | SDC_TOPOLOGY_EXTEND);
             let mut snapshot = Vec::new();
-            let fallbacks = [(1920, 1080), (1280, 720)];
+            let fallbacks = [(1920, 1080), (1280, 720), (1024, 768), (800, 600)];
 
             for i in 0..64 {
                 let mut device: DISPLAY_DEVICEW = mem::zeroed();
@@ -72,7 +42,7 @@ impl DFEngine {
                 dm.dmSize = mem::size_of::<DEVMODEW>() as u16;
 
                 if !EnumDisplaySettingsW(pcw_name, ENUM_REGISTRY_SETTINGS, &mut dm).as_bool() {
-                    let _ = EnumDisplaySettingsW(pcw_name, ENUM_CURRENT_SETTINGS, &mut dm);
+                    EnumDisplaySettingsW(pcw_name, ENUM_CURRENT_SETTINGS, &mut dm);
                 }
 
                 let is_active = (device.StateFlags & 0x1) != 0;
@@ -84,6 +54,7 @@ impl DFEngine {
                         temp_dm.dmPelsWidth = w;
                         temp_dm.dmPelsHeight = h;
                         temp_dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
+
                         let res = ChangeDisplaySettingsExW(pcw_name, Some(&temp_dm), None, CDS_UPDATEREGISTRY | CDS_NORESET, None);
                         if res == DISP_CHANGE_SUCCESSFUL || res == DISP_CHANGE_RESTART { break; }
                     }
@@ -99,13 +70,41 @@ impl DFEngine {
                 if !was_active {
                     reset_dm.dmPelsWidth = 0;
                     reset_dm.dmPelsHeight = 0;
-                    reset_dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
-                    let _ = ChangeDisplaySettingsExW(pcw_name, Some(&reset_dm), None, CDS_UPDATEREGISTRY | CDS_NORESET, None);
                 }
+                reset_dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
+                ChangeDisplaySettingsExW(pcw_name, Some(&reset_dm), None, CDS_UPDATEREGISTRY | CDS_NORESET, None);
             }
             
             self.commit_registry();
             inventory
+        }
+    }
+
+    pub fn atomic_deploy(&self, tasks: Vec<DisplayTask>, clones: Vec<(String, String)>) {
+        self.log("SYNC", "ATOMIC_COMMIT_START");
+        let inv = self.full_scan_discovery();
+
+        if !clones.is_empty() {
+            unsafe {
+                let _ = SetDisplayConfig(None, None, SDC_APPLY | SDC_TOPOLOGY_CLONE);
+            }
+        }
+
+        let mut sorted_tasks = tasks.clone();
+        sorted_tasks.sort_by_key(|t| !t.is_primary);
+
+        let mut staged_count = 0;
+        for task in &sorted_tasks {
+            if let Some(dev) = inv.iter().find(|r| self.match_display(r, &task.query)) {
+                if self.stage_config(&dev.name_id, task) {
+                    staged_count += 1;
+                }
+            }
+        }
+
+        if staged_count > 0 || !clones.is_empty() {
+            self.commit_registry();
+            self.log("OK", "ATOMIC_COMMIT_SUCCESS");
         }
     }
 
@@ -117,24 +116,47 @@ impl DFEngine {
     }
 
     fn commit_registry(&self) {
-        unsafe { let _ = ChangeDisplaySettingsExW(None, None, None, CDS_TYPE(0), None); }
+        unsafe { ChangeDisplaySettingsExW(None, None, None, CDS_TYPE(0), None); }
     }
 
-    fn force_set_registry(&self, name: &str, w: i32, h: i32, x: i32, y: i32, primary: bool) {
+    fn stage_config(&self, name: &str, task: &DisplayTask) -> bool {
         let name_u16: Vec<u16> = name.encode_utf16().chain(iter::once(0)).collect();
-        let pcw_name = PCWSTR(name_u16.as_ptr());
         unsafe {
             let mut dm: DEVMODEW = mem::zeroed();
             dm.dmSize = mem::size_of::<DEVMODEW>() as u16;
-            if EnumDisplaySettingsW(pcw_name, ENUM_REGISTRY_SETTINGS, &mut dm).as_bool() {
+            
+            if EnumDisplaySettingsW(PCWSTR(name_u16.as_ptr()), ENUM_CURRENT_SETTINGS, &mut dm).as_bool() {
+                let rotation = match task.direction.as_deref() {
+                    Some("90") => DMDO_90,
+                    Some("180") => DMDO_180,
+                    Some("270") => DMDO_270,
+                    _ => DMDO_DEFAULT,
+                };
+
+                let (w, h) = if rotation == DMDO_90 || rotation == DMDO_270 {
+                    (task.height, task.width)
+                } else {
+                    (task.width, task.height)
+                };
+
                 dm.dmPelsWidth = w as u32;
                 dm.dmPelsHeight = h as u32;
-                dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_POSITION;
-                dm.Anonymous1.Anonymous2.dmPosition.x = x;
-                dm.Anonymous1.Anonymous2.dmPosition.y = y;
+                dm.dmFields |= DM_PELSWIDTH | DM_PELSHEIGHT | DM_POSITION | DM_DISPLAYORIENTATION;
+                dm.Anonymous1.Anonymous2.dmPosition.x = task.x;
+                dm.Anonymous1.Anonymous2.dmPosition.y = task.y;
+                dm.Anonymous1.Anonymous2.dmDisplayOrientation = rotation;
+
+                if task.freq > 0 {
+                    dm.dmDisplayFrequency = task.freq;
+                    dm.dmFields |= DM_DISPLAYFREQUENCY;
+                }
+
                 let mut flags = CDS_UPDATEREGISTRY | CDS_NORESET;
-                if primary { flags |= CDS_SET_PRIMARY; }
-                let _ = ChangeDisplaySettingsExW(pcw_name, Some(&dm), None, flags, None);
+                if task.is_primary { flags |= CDS_SET_PRIMARY; }
+
+                ChangeDisplaySettingsExW(PCWSTR(name_u16.as_ptr()), Some(&dm), None, flags, None) == DISP_CHANGE_SUCCESSFUL
+            } else {
+                false
             }
         }
     }
